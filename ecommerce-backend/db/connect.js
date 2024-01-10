ff618130da95939ec3c61d71e5ed56507e7b65d7
Farook <farook.jintha@ace-alternatives.com>
2024-01-10 20:57:42 +0530

    modified the db connection

--- ecommerce-backend/db/connect.js
+++ ecommerce-backend/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try{
-        await mongoose.connect('mongodb://localhost:27017/ecommerce');
+        await mongoose.connect('mongodb+srv://farookjintha:Welcome123@ecommerce-db.p2sfdjs.mongodb.net/?retryWrites=true&w=majority');
         console.log('Database connection established.')
     }catch(error){
         console.log('Error while connecting DB: ', error)
